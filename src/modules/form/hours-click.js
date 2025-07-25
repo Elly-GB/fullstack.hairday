86408fe04fa5951114b1d8736678953c8032b96d
Gaby <gabyfranbublitz@gmail.com>
2025-07-25 20:23:49 -0300

    added hour-selected changes

--- src/modules/form/hours-click.js
+++ src/modules/form/hours-click.js
@@ -1,4 +1,15 @@
 export function hoursClick() {
   const hours = document.querySelectorAll(".hour-available")
-  
+
+  hours.forEach(( available ) => {
+    available.addEventListener("click", (selected) => {
+
+      // Remove a Class dos não-selecionados...
+      hours.forEach((hour) => {
+        hour.classList.remove("hour-selected")
+      })
+      // Adiciona a Class dos selecionados...
+      selected.target.classList.add("hour-selected")
+    })
+  })
 }
