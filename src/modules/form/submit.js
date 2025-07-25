91cce192097a749dd38511cce8971c53b9c49c5a
Gaby <gabyfranbublitz@gmail.com>
2025-07-25 17:43:16 -0300

    added better dates

--- src/modules/form/submit.js
+++ src/modules/form/submit.js
@@ -1,6 +1,17 @@
+// ...import...
+import dayjs from "dayjs"
 
+// ...const...
 const form = document.querySelector("form")
+const selectedDate = document.getElementById("date")
 
+const inputToday = dayjs(new Date()).format("YYYY-MM-DD") // Easy Peasy Lemon Squeezy !
+
+// ...date...
+selectedDate.value = inputToday // Data atual no agendamento...
+selectedDate.min = inputToday // -> Data atual = Mínima
+
+// ...form...
 form.onsubmit = (event) => {
   event.preventDefault()
 
