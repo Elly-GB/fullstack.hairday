38ed861a07e4b64fe60824c4a89a87dc23506575
Gaby <gabyfranbublitz@gmail.com>
2025-07-25 19:35:24 -0300

    added hour.split

--- src/modules/form/hours-load.js
+++ src/modules/form/hours-load.js
@@ -1,5 +1,13 @@
 import { openingHours } from "../../utils/opening-hours.js"
 
+// ...hours...
 export function hoursLoad() {
-  
+  const opening = openingHours.map((hour) => {
+    const [scheduleHour] = hour.split(":") // Faz o ":" se tornar um divisor (9:00 -> [9], [00])
+
+    
+    console.log(scheduleHour)
+  })
 }
+
+// Recupera somente os horários.
