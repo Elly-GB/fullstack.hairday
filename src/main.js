4e0ae040d77c3616a40bb294e79466613d6c4f06
Gaby <gabyfranbublitz@gmail.com>
2025-07-25 15:13:40 -0300

    added import dayjs

--- src/main.js
+++ src/main.js
@@ -1,6 +1,13 @@
 "use strict"
 
+// dayjs - config
+import "./libs/dayjs.js"
+
 // css
 import "./styles/global.css"
 import "./styles/form.css"
 import "./styles/schedule.css"
+
+//
+import dayjs from "dayjs"
+console.log(dayjs().format("DD/MM - HH:mm"))
