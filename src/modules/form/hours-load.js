6f6238542d203f2b030258007ef27cd1b25b7329
Gaby <gabyfranbublitz@gmail.com>
2025-07-25 20:03:07 -0300

    added html hours

--- src/modules/form/hours-load.js
+++ src/modules/form/hours-load.js
@@ -2,10 +2,13 @@ import dayjs from "dayjs"
 
 import { openingHours } from "../../utils/opening-hours.js"
 
+// ...const...
+const hours = document.getElementById("hours")
+
 // ...hours...
 export function hoursLoad({ date }) {
   const opening = openingHours.map((hour) => {
-    // Recupera somente as horas...
+    // Recupera somente os horários...
     const [scheduleHour] = hour.split(":") // Faz o ":" se tornar um divisor (9:00 -> [9], [00])
 
     // Add 'hour' na 'date' e verifica se a data é depois da atual...
@@ -16,8 +19,17 @@ export function hoursLoad({ date }) {
       hour,
       available: isHourPast,
     }
-
   })
-}
 
-// Recupera somente os horários.
+  // Renderizando o HTML dos horários...
+  opening.forEach(({ hour, available }) => {
+    const li = document.createElement("li")
+
+    li.classList.add("hour")
+    li.classList.add(available ? "hour-available" : "hour-unavailable")
+
+    li.textContent = hour
+    hours.append(li)
+  })
+
+}
