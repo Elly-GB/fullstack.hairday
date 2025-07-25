a500859db6a06320f5d0c97388201152dd50ce9c
Gaby <gabyfranbublitz@gmail.com>
2025-07-25 19:46:29 -0300

    added hoursLoad

--- src/modules/schedules/load.js
+++ src/modules/schedules/load.js
@@ -1,5 +1,10 @@
 import { hoursLoad } from "../form/hours-load.js"
 
+// ...const...
+const selectedDate = document.getElementById("date") // -> input da data
+
 export function schedulesDay() {
-  hoursLoad() // Renderiza as horas disponíveis.
+  const date = selectedDate.value // Obtém a Data selecionada (input).
+
+  hoursLoad({ date }) // Renderiza as horas disponíveis.
 }
