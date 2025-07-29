e309a9e2de11d6620f07a20e165e553f6fd16e95
Gaby <gabyfranbublitz@gmail.com>
2025-07-29 09:14:46 -0300

    added dailySchedules content

--- src/modules/schedules/show.js
+++ src/modules/schedules/show.js
@@ -7,6 +7,25 @@ const periodNight = document.getElementById("period-night")
 
 export function schedulesShow({ dailySchedules }) {
   try {
+    // ...Washing...
+    periodMorning.innerHTML = ""
+    periodAfternoon.innerHTML = ""
+    periodNight.innerHTML = ""
+
+    // to HTML...
+    dailySchedules.forEach((schedule) => {
+      // ITEM (li)
+      const item = document.createElement("li")
+      item.setAttribute("data-id", schedule.id) // ID do agendamento.
+      // TIME (strong)
+      const time = document.createElement("strong")
+      time.textContent = dayjs(schedule.when).format("HH:mm")
+      // NAME (span)
+      const name = document.createElement("span")
+      name.textContent = schedule.name
+     
+
+    });
     
   } catch (error) {
     alert("Não foi possível exibir os agendamentos")
