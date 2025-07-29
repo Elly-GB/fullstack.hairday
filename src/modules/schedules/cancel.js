f2e09fa1869009f445efa73748b0b70af0d1b148
Gaby <gabyfranbublitz@gmail.com>
2025-07-29 10:09:13 -0300

    added periods

--- src/modules/schedules/cancel.js
+++ src/modules/schedules/cancel.js
@@ -1,1 +1,21 @@
 const periods = document.querySelectorAll(".period")
+
+// Click...
+periods.forEach((period) => {
+  period.addEventListener("click", (event) => {
+    // Cancel Icon
+    if(event.target.classList.contains("cancel-icon")) {
+
+      const item = event.target.closest("li") // Elemento pai (closest)
+      const { id } = item.dataset
+
+      if(id) {
+        const isConfirm = confirm ("Você tem certeza que deseja cancelar o seu agendamento?")
+
+        if(isConfirm) {
+          console.log ("Agendamento removido.")
+        }
+      }
+    }
+  })
+})
